5b43fce94d4729e69f017088702b617d52b4ffe5
Edward Conn <cardseller2@gmail.com>
2024-07-23 21:49:49 -0400

    Stub out dictionary tests

---
+++ src/test/java/com/famiforth/UserDictionaryTest.java
@@ -0,0 +1,27 @@
+package com.famiforth;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class UserDictionaryTest {
+    
+    UserDictionary dictionary;
+
+    @Test
+    public void populateDictionaryTest() throws IOException {
+        
+    }
+
+    @Test
+    public void addWordTest() throws IOException {
+        
+    }
+
+    @Test
+    public void getDefinitionTest() throws IOException {
+        
+    }
+
+    
+}
