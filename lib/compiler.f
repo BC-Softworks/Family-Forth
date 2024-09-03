0fd74607177163bea3ac1a99268662be57ec1d1a
Edward Conn <cardseller2@gmail.com>
2024-09-02 21:44:40 -0400

    Add random library

--- lib/compiler.f
+++
@@ -1,7 +0,0 @@
-\ Core compiler words
-\ author: Edward Conn
-
-\ Defines the following core words
-\ CREATE CONSTANT VARIABLE
-
-require "memory.f"
---
+++ lib/random.f
@@ -0,0 +1,17 @@
+\ NES RNG library using an XORshift generator
+\ Author: Edward Conn
+
+require "memory.f"
+
+variable seed
+
+: setseed ( n -- ) seed ! ;
+
+: rnd ( -- n ) 
+    seed @             ( Push the value at seed to TOS )
+    DUP 07 LSHIFT XOR
+    DUP 09 RSHIFT XOR
+    DUP 13 LSHIFT XOR  ( Generate the next seed )
+    DUP seed ! ;       ( Duplicate the result and store a copy )
+
+: random ( n -- n ) RND SWAP MOD ;
